39a3daf87579962fea887bde5a8e710259039484
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-21 17:23:38 +0530

    server

--- server.js
+++ server.js
@@ -3,18 +3,11 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 
+const PORT = process.env.PORT || 4000;
 const app = express();
+await connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 
-app.get("/", async (req, res) => {
-  try {
-    await connectDB();
-    res.send("API WORKING !!!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    res.status(500).send("Database connection failed");
-  }
-});
-
-export default app;
+app.get("/", (req, res) => res.send(`API WORKING !!!`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
