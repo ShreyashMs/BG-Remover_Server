49755529c22a34263dfedebbc651471e25abe38d
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-21 17:09:18 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,21 +3,11 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 
-const app = express();
 const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
+app.use(express.json());
+app.use(cors())
 
-async function startServer() {
-  await connectDB();
-
-  app.use(express.json());
-  app.use(cors());
-
-  app.get("/", (req, res) => res.send("API WORKING !!!"));
-
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-// Start the server
-startServer().catch((err) => {
-  console.error("Failed to start server:", err);
-});
+app.get("/", (req, res) => res.send(`API WORKING !!!`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
