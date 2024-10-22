a7c00c667713221bbde37a9cca66f7e003f5c425
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-22 16:14:48 +0530

    Webhook added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -10,4 +11,6 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/", (req, res) => res.send(`API WORKING !!!`));
+app.use('/api/user',userRouter)
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
