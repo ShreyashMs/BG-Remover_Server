d22cb606d963eb44605b1401ecce07a4b47c0719
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-22 16:42:44 +0530

    database collection changes

--- configs/mongodb.js
+++ configs/mongodb.js
@@ -4,7 +4,11 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/bg-remover`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/bg-remover`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("database connected!!!")
 };
 
 export default connectDB;
