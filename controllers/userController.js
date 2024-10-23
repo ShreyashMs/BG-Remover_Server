091d9a99e27473940e172bdc8a1afdd54064c189
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-23 14:30:13 +0530

    Test

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ import userModel from "../models/userModel.js";
 
 // Manage clerk user
 const clerkWebhooks = async (req, res) => {
+  console.log("Received webhook:", req.body);
+  console.log("Headers:", req.headers);
+
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOKS_SECRET);
     await whook.verify(JSON.stringify(req.body), {
@@ -16,15 +19,11 @@ const clerkWebhooks = async (req, res) => {
     switch (type) {
       case "user.created": {
         const userData = {
-          clerkId: data.id,
-          email: data.email_addresses[0].email_address,
-          firstName: data.first_name,
-          lastName: data.last_name,
-          photo: data.image_url,
+         
         };
-        await userModel.create(userData);
-        console.log("Webhook received:", req.body);
-        return res.status(201).json({});
+        const newUser = await userModel.create(userData);
+        console.log("User created:", newUser);
+        return res.status(201).json({ success: true });
       }
       case "user.updated": {
         const userData = {
@@ -39,23 +38,24 @@ const clerkWebhooks = async (req, res) => {
           { new: true }
         );
         if (!updatedUser) {
-          return res
-            .status(404)
-            .json({ success: false, message: "User not found" });
+          return res.status(404).json({ success: false, message: "User not found" });
         }
-        return res.json({});
+        console.log("User updated:", updatedUser);
+        return res.json({ success: true });
       }
       case "user.deleted": {
-        await userModel.findOneAndDelete({ clerkId: data.id });
-        return res.json({});
+        const deletedUser = await userModel.findOneAndDelete({ clerkId: data.id });
+        if (!deletedUser) {
+          return res.status(404).json({ success: false, message: "User not found" });
+        }
+        console.log("User deleted:", deletedUser);
+        return res.json({ success: true });
       }
       default:
-        return res
-          .status(400)
-          .json({ success: false, message: "Unknown event type" });
+        return res.status(400).json({ success: false, message: "Unknown event type" });
     }
   } catch (error) {
-    console.log(error.message);
+    console.error("Error processing webhook:", error); 
     return res.status(400).json({ success: false, message: error.message });
   }
 };
